928962cd346c5a5489e7a3a9f2a379f654d0f111
vikash-pandey1 <vikashpandey7082@gmail.com>
2023-12-24 23:37:13 +0530

    datatype

--- 01basic/01_variable.js
+++ 01basic/01_variable.js
@@ -2,5 +2,16 @@ const accountId = 343424
 let accountEmail = "vikash@123.com"
 var accountPassword = "2343"
 accountCity = "Mathura"
+let accountState;
+accountEmail = "faslf"
+accountPassword = "343"
+accountCity = "agra"
 
-accountId = 3
+/*
+prefer not to use var 
+becaouse of issue in block and function scope
+*/ 
+// accountId = 3
+console.log(accountId);
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 02datatype/ram.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version\
+
+// alert("hello") we are using node.js not browser
+
+console.log(23-53);  // code readability should be high
+
+console.log('viaks');
+
+let name = "vikash";
+let age = 22;
+let isLoggedIn = true;
+
+// number -> 2^53
+// bigint -> 2^
+// string -> "dsfa",'dsf'
+//boolean -> true,false
+// undefine ->
+// null -> standalone value
+// symbol -> unique
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
