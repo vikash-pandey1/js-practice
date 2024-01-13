f7044b8a24f8d6e2e810d41898bb29b2b4a5b598
vikash-pandey1 <vikashpandey7082@gmail.com>
2024-01-13 12:24:41 +0530

    add math and num file

---
+++ 01basic/06_nums_math.js
@@ -0,0 +1,32 @@
+
+// ++++++++++++++++++++++number ++++++++++++
+/*const score = 500
+console.log(typeof score);
+
+const balance = new Number(100.3333)
+console.log(balance);
+console.log( typeof balance.toString());
+console.log( typeof balance);
+console.log(balance.toFixed(2));
+
+const no = 23.53535
+console.log(no.toPrecision(3));
+
+const hundreds = 100000000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+console.log(Number.MAX_VALUE);*/
+
+// ++++++++++++ Maths ++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-5));
+console.log(Math.floor(Math.random()*10)+10);
+// console.log(Math.round(5.3));
+// console.log(Math.ceil(5.1));
+// console.log(Math.floor(5.9));
+// console.log(Math.min(2,53));
+// console.log(Math.max(53,35));
+
+// console.log(Math.random() * ());
