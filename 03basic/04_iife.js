a4d3bc2ef7bbaf5f298afa34e89861dfbdbfb1a0
vikash-pandey1 <vikashpandey7082@gmail.com>
2024-01-23 22:09:41 +0530

    add IIFE

---
+++ 03basic/04_iife.js
@@ -0,0 +1,14 @@
+// immediately invoked function expresson (IIFE)
+
+(function chai(name){
+    // named iife
+    console.log(`db connected in js ${name}`);
+})('vikash');
+
+(()=>{
+    console.log('db connected');
+})()
+// some times problem is created by global variable so 
+//to get rid from global scopre we use 
+// to remove the global scope polution(variable) we use iife
+
